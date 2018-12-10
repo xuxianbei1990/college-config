785de3bb94049abd3ea0798065c4683e2f2a7bab
许贤贝 <909111761@qq.com>
2018-12-10 19:22:06 +0800

    Update src/main/java/college/Filter/MyFilter.java
    添加过滤器

--- src/main/java/college/Filter/MyFilter.java
+++ src/main/java/college/Filter/MyFilter.java
@@ -8,12 +8,14 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
- * Name
+ * Name  过滤器
  *
  * @author xuxb
  * Date 2018-12-10
  * VersionV1.0
- * @description
+ * @description  https://www.cnblogs.com/chenhonggao/p/9027591.html
+ *
+ *  过滤器原理
  */
 @Component
 public class MyFilter implements Filter {
